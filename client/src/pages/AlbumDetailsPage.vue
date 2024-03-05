383d306693bb17b57f525c93e1f3836dd9161cd1
Jeremy Fowler <jembyfowler@gmail.com>
2024-03-05 14:27:41 -0700

    got pictures

--- client/src/pages/AlbumDetailsPage.vue
+++ client/src/pages/AlbumDetailsPage.vue
@@ -24,15 +24,26 @@ import { computed, onMounted } from 'vue';
 import { useRoute } from 'vue-router'
 import Pop from '../utils/Pop.js'
 import { albumsService } from '../services/AlbumsService.js'
+import { picturesService } from '../services/PicturesService.js';
 export default {
   setup() {
     const route = useRoute()
     onMounted(() => {
       getAlbumById()
+      getPicturesByAlbumId()
     })
+
     async function getAlbumById() {
       try {
         await albumsService.getAlbumById(route.params.albumId)
+      } catch (error) {
+        Pop.error(error)
+      }
+    }
+
+    async function getPicturesByAlbumId() {
+      try {
+        await picturesService.getPicturesByAlbumId(route.params.albumId)
       } catch (error) {
         Pop.error(error)
       }
